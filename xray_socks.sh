c6c20ee78d2d373d48ffac70d529bb5d980f0692
Tony855 <58850110+Tony855@users.noreply.github.com>
2025-02-11 22:28:23 +0700

    Update xray_socks.sh

--- xray_socks.sh
+++ xray_socks.sh
@@ -17,7 +17,7 @@ fi
 install_xray() {
     echo "安装 Xray..."
     apt-get install unzip -y || yum install unzip -y
-    wget https://github.com/XTLS/Xray-core/releases/download/v25.1.1/Xray-linux-64.zip
+    wget https://github.com/XTLS/Xray-core/releases/download/v25.1.30/Xray-linux-64.zip
     unzip Xray-linux-64.zip
     mv xray /usr/local/bin/xrayL
     chmod +x /usr/local/bin/xrayL
